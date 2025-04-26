dcd4a5ae2ad85f56e1f60409fa8669ab887b5917
sebiten <sebdevspace@gmail.com>
2025-04-26 14:43:27 -0300

    styles

--- components/hero.tsx
+++ components/hero.tsx
@@ -67,9 +67,13 @@ export default function Home() {
                 <span>Optimizado para ATS</span>
               </div>
             </div>
-            <div className="flex-1 relative">
+            <div className="flex-1 relative mt-2 ">
               <div className="relative bg-[#1F1F22] rounded-lg p-1 shadow-2xl shadow-[#7C3AED]/20">
-              
+                <img
+                  src="/ejemplocv111.webp"
+                  alt="CV generado por VitaeSpark"
+                  className="rounded-lg w-full"
+                />
                 <div className="absolute -top-4 -right-4 bg-[#38BDF8] text-[#0F0F10] font-bold px-4 py-2 rounded-full text-sm">
                   ¡Optimizado para ATS!
                 </div>
