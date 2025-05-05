ef43331d27d767423f876bd506f734ba8686c4eb
sebiten <sebdevspace@gmail.com>
2025-05-05 18:59:51 -0300

    sitemap done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
-import type { DatosCVFormulario, RespuestaCV } from "@/lib/types/cv";
-import { DocumentoCV } from "@/components/pdf/CVDocument";
-import { motion } from "framer-motion";
-import CVForm from "@/components/pdf/CVForm";
 import Hero from "@/components/hero";
 
 export default function GeneradorCV() {
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://vitaespark.com",
-    generateRobotsTxt: true,
-    changefreq: "daily",
-    priority: 0.7,
-    exclude: ["/login", "/api/*", "/admin/*"],
-    robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: "*",
-          allow: "/",
-        },
-      ],
-    },
-  }
-  
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://vitaespark.com",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/login", "/api/*", "/admin/*"],
+  sourceDir: ".next", // 👈 explícitamente le decimos dónde está el build
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+};
