6a872c723e924bc4afd8fec9fe831355c357bb53
sebiten <sebdevspace@gmail.com>
2025-05-05 18:48:09 -0300

    robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || "https://vitaespark.com",
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://vitaespark.com",
     generateRobotsTxt: true,
     changefreq: "daily",
     priority: 0.7,
