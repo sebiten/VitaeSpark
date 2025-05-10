c9209f1332b3eab89f6192853bfff81eadf33601
sebiten <sebdevspace@gmail.com>
2025-05-10 17:42:47 -0300

    fixed - .tsx on import

--- components/pdf/CVDocument.tsx
+++ components/pdf/CVDocument.tsx
@@ -2,7 +2,7 @@ import type { RespuestaCV } from "@/lib/types/cv"
 import BlueTemplate from "./template/BlueTemplate"
 import GreenTemplate from "./template/GreenTemplate"
 import PurpleTemplate from "./template/PurpleTemplate"
-import HarvardTemplate from "./template/harvard-cv-template.tsx";
+import HarvardTemplate from "./template/harvard-cv-template"
 
 export function DocumentoCV({ cv, template = "harvard" }: { cv: RespuestaCV["cv"]; template?: string }) {
   // Renderizar el template seleccionado
