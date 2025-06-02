46c0951234ac21646b8418dcb3151de23dfecf24
sebiten <sebdevspace@gmail.com>
2025-06-02 16:44:20 -0300

    done

--- components/CVFormStep.tsx
+++ components/CVFormStep.tsx
@@ -198,7 +198,7 @@ export default function CVFormStep({
             </div>
           </div>
         </motion.div>
-        <div className="w-full flex items-center justify-center ">
+        {/* <div className="w-full flex items-center justify-center ">
           <Button
             type="button"
             onClick={rellenarDatosPrueba}
@@ -208,7 +208,7 @@ export default function CVFormStep({
             <CheckCircle2 className="w-4 h-4 mr-1.5 text-[#7C3AED]" />
             Rellenar con datos de prueba
           </Button>
-        </div>
+        </div> */}
 
         {/* Datos personales */}
         <div className="bg-[#2A2A2D]/50 p-3 rounded-lg border border-[#3F3F46]/30">
