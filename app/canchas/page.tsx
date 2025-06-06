0a11ac5adce026b3184d73f0b570bace3653b695
sebiten <sebdevspace@gmail.com>
2025-06-06 11:10:17 -0300

    letssee

---
+++ app/canchas/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h2>canchas</h2>
+    </div>
+  );
+};
+
+export default Page;
